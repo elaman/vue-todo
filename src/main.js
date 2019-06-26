4088df6489a4ab8d38bd7f3b9147b65a49c87969
Elaman Imashov <elaman@outlook.com>
2019-06-27 00:38:06 +0600

    Create api service using axios.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ApiService from "./services/api.service";
+import { TokenService } from "./services/token.service";
+
+ApiService.init("https://dev-d8-charts.pantheonsite.io");
+
+if (TokenService.getToken()) {
+  ApiService.setHeader();
+}
 
 Vue.config.productionTip = false;
 
