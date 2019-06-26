8c018b3e3b47c28e27ab211fe6b4bc6b3e6bf489
Elaman Imashov <elaman@outlook.com>
2019-06-27 00:32:27 +0600

    Create token service to store session tokens.

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import LoginView from "./views/LoginView.vue";
-import { TokenService } from "./services/storage.service";
+import { TokenService } from "./services/token.service";
 
 Vue.use(Router);
 
