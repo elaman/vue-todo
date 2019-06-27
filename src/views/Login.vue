125a52fe25fd7e6fe58b356aed31576e261923b9
Elaman Imashov <elaman@outlook.com>
2019-06-27 23:16:53 +0600

    Create vue files

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit="handleSubmit">
+    <label for="username">
+      Username:
+      <input id="username" v-model="username">
+    </label>
+    <label for="password">
+      Password:
+      <input id="password" v-model="password">
+    </label>
+    <input type="submit" value="Submit">
+  </form>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  computed: {
+    ...mapGetters("auth", [
+      "authenticating",
+      "authenticationError",
+      "authenticationErrorCode"
+    ])
+  },
+  methods: {
+    ...mapActions("auth", ["login"]),
+    handleSubmit() {
+      if (this.username !== "" && this.password !== "") {
+        this.login({ username: this.username, password: this.password });
+        this.password = "";
+      }
+    }
+  }
+};
+</script>
