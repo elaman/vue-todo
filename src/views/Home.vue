e245dc4578ccaa0c7e755a65892255bd7764ad77
Elaman Imashov <elaman@outlook.com>
2019-06-30 00:34:07 +0600

    Add logout button.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Home</div>
+  <div>
+    <h1>Home</h1>
+
+    <button v-if="loggedIn" @click.prevent="logout">Logout</button>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "home",
+
+  computed: {
+    ...mapGetters("auth", ["loggedIn"])
+  },
+  
+  methods: {
+    ...mapActions("auth", ["logout"])
+  }
+};
+</script>
