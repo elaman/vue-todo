9e8ac1d5976e136ace92ef7b3cd3dde8c77d2b5f
Elaman Imashov <elaman@outlook.com>
2019-06-30 11:29:03 +0600

    Create CompletedTodos.vue file.

---
+++ src/components/CompletedTodos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      Current
+      <span class="badge badge-info">{{completedTodos.length}}</span>
+    </div>
+    <div v-for="todo in completedTodos" v-bind:key="todo.id" class="card-body">
+      <h5 class="card-title">{{todo.body}}</h5>
+      <div class="btn-group btn-group-sm mr-2" role="group" aria-label="Operations">
+        <button @click="removeTodo(todo)" type="button" class="btn btn-outline-danger">Remove</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  methods: { ...mapActions("todo", ["removeTodo"]) },
+  computed: { ...mapGetters("todo", ["completedTodos"]) }
+};
+</script>
